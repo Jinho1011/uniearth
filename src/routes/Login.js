b80374ee1f028729d8d5df7162e0f780108da1c5
jinho1011 <gino9940@gmail.com>
2021-08-14 10:57:05 +0900

    Add login inputs

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,9 +1,33 @@
 import React, { useState, useEffect } from "react";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    id: "",
+    pwd: "",
+  });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const submit = async () => {};
+
   return (
     <>
       <h1>Login</h1>
+      <div>
+        아이디
+        <input name="id" onChange={onChange} value={inputs.id}></input>
+      </div>
+      <div>
+        비밀번호
+        <input name="pwd" onChange={onChange} value={inputs.pwd}></input>
+      </div>
+      <button onClick={submit}>Sign Up</button>
     </>
   );
 };
