1efc76a8628e5ad40147a83c03596a7a5a4d0305
jinho1011 <gino9940@gmail.com>
2021-08-14 20:43:58 +0900

    Add background

--- src/routes/Feed.js
+++ src/routes/Feed.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import jwt_decode from "jwt-decode";
 
+import { Background } from "../components/Background";
 import Earth from "../components/Earth";
 import Writer from "../components/Writer";
 import Header from "../components/Header";
+
 const Feed = () => {
   const [token, setToken] = useState({});
 
@@ -17,12 +19,12 @@ const Feed = () => {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <>
+    <Background>
       <Header></Header>
       <Earth></Earth>
       <button onClick={() => setShowModal(true)}>글쓰기</button>
       <Writer showModal={showModal} setShowModal={setShowModal} token={token} />
-    </>
+    </Background>
   );
 };
 
