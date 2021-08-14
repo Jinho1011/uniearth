074d8f5ca0ec9c5d5cec1b9beb82bce08fe20adf
jinho1011 <gino9940@gmail.com>
2021-08-14 22:01:50 +0900

    Change to react-globe.gl

--- src/components/Earth.js
+++ src/components/Earth.js
@@ -1,7 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Globe from "react-globe.gl";
 
 const Earth = () => {
-  return <></>;
+  const globeEl = useRef();
+  let earth = null;
+  let camera = null;
+  let scene = null;
+  let canvas = null;
+  let controls = null;
+
+  useEffect(() => {
+    setTimeout(() => {
+      canvas = globeEl.current;
+      scene = canvas.scene();
+      camera = canvas.camera();
+      earth = scene.children[3];
+      controls = canvas.controls();
+      controls.autoRotate = true;
+      controls.autoRotateSpeed = 2;
+    }, 1000);
+    camera = globeEl.current.camera();
+  }, []);
+
+  return (
+    <Globe
+      ref={globeEl}
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      backgroundImageUrl=""
+      pointAltitude="size"
+      pointColor="color"
+      animateIn={false}
+    />
+  );
 };
 
 export default Earth;
