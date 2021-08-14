9fae759a8bb592105dbb03da9c4c08fe5a6d9058
jinho1011 <gino9940@gmail.com>
2021-08-15 06:41:01 +0900

    Fix

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,12 +4,19 @@ import "../styles/menu.css";
 import "../styles/form.css";
 
 const Menu = ({ setShowModal, continents }) => {
-  console.log(continents);
+  useEffect(() => {
+    console.log(continents);
+  }, [continents]);
+
   return (
     <div className="container is-max-desktop menu">
       <div className="menu-left">
         <button className="menu-button">현재 위치를 경도로 설정</button>
-        <button className="menu-button">{continents}</button>
+        {/* {continents.length ? (
+          <button className="menu-button">
+            {continents.map((c) => c + "  ")}
+          </button>
+        ) : null} */}
       </div>
       <div className="menu-right">
         <button className="menu-button" onClick={() => setShowModal(true)}>
