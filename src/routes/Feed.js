789ff48877db0567ae89eeb60f85c08a50a7c0fa
jinho1011 <gino9940@gmail.com>
2021-08-14 20:17:49 +0900

    Save JWT

--- src/routes/Feed.js
+++ src/routes/Feed.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
 
 import Earth from "../components/Earth";
 import Writer from "../components/Writer";
 
 const Feed = () => {
+  const [token, setToken] = useState({});
+
+  useEffect(() => {
+    let JWT = window.sessionStorage.getItem("JWT");
+    setToken(JWT);
+  }, []);
+
+  useEffect(() => {}, [token]);
+
   const [showModal, setShowModal] = useState(false);
 
   return (
@@ -11,7 +21,7 @@ const Feed = () => {
       <h1>Feed</h1>
       <Earth></Earth>
       <button onClick={() => setShowModal(true)}>글쓰기</button>
-      <Writer showModal={showModal} setShowModal={setShowModal}></Writer>
+      <Writer showModal={showModal} setShowModal={setShowModal} token={token} />
     </>
   );
 };
