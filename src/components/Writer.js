bf7cb56f2ac7114cd8223d1396fd102640a168e2
Dong <sdg2435@gmail.com>
2021-08-14 12:00:11 +0900

    Add Dropzone

--- src/components/Writer.js
+++ src/components/Writer.js
@@ -1,17 +1,3 @@
-import { useState, useEffect, useRef } from "react";
-import Modal from "react-modal";
-
-Modal.setAppElement(document.getElementById("root"));
-
-const Writer = ({ showModal, setShowModal }) => {
-  return (
-    <Modal isOpen={showModal} contentLabel="Example Modal">
-      HIdd
-    </Modal>
-  );
-};
-
-export default Writer;
 import React, { useState, useEffect, useRef } from "react";
 import Modal from "react-modal";
 import Dropzone from "./Dropzone";
