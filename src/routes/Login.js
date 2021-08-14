a8c88c0937b37894e686291b8ff10dfe88b44f18
jinho1011 <gino9940@gmail.com>
2021-08-14 13:37:06 +0900

    Fix error handle

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -39,23 +39,22 @@ const Login = () => {
     };
 
     return fetch("/auth/login", requestOptions)
-      .then((response) => response.text())
+      .then((response) => {
+        return { code: response.status, data: response.text() };
+      })
       .then((result) => result)
       .catch((error) => error);
   };
 
   const submit = async () => {
-    let res = await login();
-    res = JSON.parse(res);
-
-    if (res.code === 200) {
-      // success
-      let decoded = jwt_decode(res.token);
+    let { code, data } = await login();
+    if (code === 200) {
+      data = JSON.parse(await data);
+      let decoded = jwt_decode(data.token);
       window.sessionStorage.setItem("JWT", decoded);
-
       setDone(true);
     } else {
-      // error
+      alert("login failed");
     }
   };
 
