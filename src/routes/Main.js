169993214dc71f184f6c13bca0adad84bb6f3d67
jinho1011 <gino9940@gmail.com>
2021-08-14 15:00:42 +0900

    Add Header

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import "bulma/css/bulma.min.css";
+
+import Header from "../components/Header";
+
+const Background = styled.div`
+  background: url(${process.env.PUBLIC_URL + "/images/background.png"});
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100vw;
+  height: 100vh;
+`;
 
 const Main = () => {
   return (
-    <>
-      <h1>Main</h1>
-    </>
+    <Background>
+      <Header></Header>
+    </Background>
   );
 };
 
