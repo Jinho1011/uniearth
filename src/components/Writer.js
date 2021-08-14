62fd1d3e7cf0b2a4a345bfd64659dacfd21d1847
Dong <sdg2435@gmail.com>
2021-08-14 11:56:39 +0900

    Test

--- src/components/Writer.js
+++ src/components/Writer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import Modal from "react-modal";
 
 Modal.setAppElement(document.getElementById("root"));
