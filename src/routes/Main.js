3b0077fc921c08c50f776b87717abfb0b0e3541a
jinho1011 <gino9940@gmail.com>
2021-08-14 15:39:48 +0900

    Make Background to component

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -3,14 +3,7 @@ import styled from "styled-components";
 import "bulma/css/bulma.min.css";
 
 import Header from "../components/Header";
-
-const Background = styled.div`
-  background: url(${process.env.PUBLIC_URL + "/images/background.png"});
-  background-size: cover;
-  background-repeat: no-repeat;
-  width: 100vw;
-  height: 100vh;
-`;
+import { Background } from "../components/Background";
 
 const Main = () => {
   return (
