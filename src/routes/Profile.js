7f99a004f736ea65830e2f9919a5b42dc7616966
jinho1011 <gino9940@gmail.com>
2021-08-14 20:18:00 +0900

    Fix path

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+import MyPosts from "../components/MyPosts";
+import MyProfile from "../components/MyProfile";
+
 const Profile = () => {
   return (
     <>
       <h1>Profile</h1>
+      <MyPosts></MyPosts>
+      <MyProfile></MyProfile>
     </>
   );
 };
