b30b1020b976040f48326534da5f099305452805
jinho1011 <gino9940@gmail.com>
2021-08-15 04:59:00 +0900

    Style Posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from "react";
 
+import "../styles/Posts.css";
+
 const Post = ({ post }) => {
   console.log("🚀 ~ file: Post.js ~ line 4 ~ Post ~ post", post);
-  return <div>post</div>;
+  return (
+    <div className="post-container">
+      <div className="content-container">
+        <div className="content-header">
+          <div className="user-container">
+            <img
+              src={process.env.PUBLIC_URL + "/images/profileImgFull.png"}
+              alt="scope"
+            />
+            <div className="user-info">
+              <div className="user-name">{post.post_owner}</div>
+              <div className="user-time">{post.post_updatestamp}</div>
+            </div>
+          </div>
+          <div className="likes-container"></div>
+        </div>
+        <div className="content-body"></div>
+      </div>
+      <div className="comment-container">asd</div>
+    </div>
+  );
 };
 
 export default Post;
