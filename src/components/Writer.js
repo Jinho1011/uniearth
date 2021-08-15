a739a9000297e526283adea39039644cd21a39c2
Dong <sdg2435@gmail.com>
2021-08-15 11:09:13 +0900

    aa

--- src/components/Writer.js
+++ src/components/Writer.js
@@ -110,6 +110,8 @@ const Writer = ({ showModal, setShowModal, token, setRefresh }) => {
     myHeaders.append("Content-Type", "application/json");
 
     //_update_seq을 리턴
+    console.log(postResult._updated_seq);
+    console.log(file);
     var raw = JSON.stringify({  
       file_post: postResult._updated_seq,
       file_owner: user.useremail,
