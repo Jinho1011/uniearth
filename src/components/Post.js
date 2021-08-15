b39b80c8c03c88292bcde0c33f4e522c2f070623
Dong <sdg2435@gmail.com>
2021-08-15 11:30:17 +0900

    a

--- src/components/Post.js
+++ src/components/Post.js
@@ -81,7 +81,7 @@ const Post = ({ user, post, refresh, setRefresh }) => {
   useEffect(() => {
     const init = async () => {
       let { res, data } = await getComments();
-      let { res2, data2 } = await getFiles();
+      let fileRes = await getFiles();
 
       if (res.status === 200) {
         data = await data;
@@ -89,12 +89,12 @@ const Post = ({ user, post, refresh, setRefresh }) => {
         setComments(data.comments);
       }
       
-      console.log("🚀 ~ file: Post.js ~ line 94 ~ init ~ res2", res2)
-      if(res2?.status === 200) {
-        data2 = await data2;
-        data2 = JSON.parse(data2);
-        console.log("🚀 ~ file: Post.js ~ line 97 ~ init ~ data2", data2)
-      }
+      console.log("🚀 ~ file: Post.js ~ line 94 ~ init ~ res2", fileRes)
+      // if(res2?.status === 200) {
+      //   data2 = await data2;
+      //   data2 = JSON.parse(data2);
+      //   console.log("🚀 ~ file: Post.js ~ line 97 ~ init ~ data2", data2)
+      // }
     };
     init();
   }, []);
