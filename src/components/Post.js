2e3a16eb3b6ebfa6136863f7f8c780db6b1261dc
Dong <sdg2435@gmail.com>
2021-08-15 11:28:19 +0900

    a

--- src/components/Post.js
+++ src/components/Post.js
@@ -74,6 +74,7 @@ const Post = ({ user, post, refresh, setRefresh }) => {
 
     return fetch("/uniearth/files/file_post/" + post.SEQ, requestOptions)
       .then((response) => {
+        console.log("🚀 ~ file: Post.js ~ line 77 ~ .then ~ response", response)
         return { res: response, data: response.text() };
       })
       .catch((error) => console.log("error", error));
